e750766b9abfce48e574b6e8b1fa607744328235
Deepak vishwakarma <vdeepak524@gmail.com>
2021-03-30 13:39:41 +0530

    deepak

---
+++ application/inc/head.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<link rel="preconnect" href="//images.thrillophilia.com" crossorigin="">
+<link rel="preconnect" href="//dmcolafwvvbhg.cloudfront.net" crossorigin="">
+<link rel="preload" href="//staging-ui.thrillophilia.com" crossorigin="">
+<link rel="preload" href="//ui-assets-gc.thrillophilia.com" crossorigin="">
+<link rel="preconnect" href="https://www.googletagmanager.com" crossorigin="">
+<link rel="preconnect" href="https://www.google-analytics.com" crossorigin="">
+<link rel="preconnect" href="https://connect.facebook.net" crossorigin="">
+<link rel="preconnect" href="https://www.facebook.com" crossorigin="">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="csrf-param" content="authenticity_token">
+<meta name="csrf-token" content="uwCbvBinwcWNK2Sp8R3nxJQaq0OcNPmCmSye39oz5uoLCROtzXS859lCQBuuonianEG6puK37vhEdo3RO7s8ZQ==">
+
+
+
+
+
+<link rel="canonical" href="https://www.thrillophilia.com/">
+
+
+  <!-- Data layer needs to come before GTM Snippet -->
+
+  <!-- Google Tag Manager -->
+  <script type="text/javascript" src="https://bam-cell.nr-data.net/1/86a6d59195?a=386753053&amp;v=1208.49599aa&amp;to=cFoITEQOXlpTQxZeWF5QSVFYBVdO&amp;rst=3695&amp;ck=1&amp;ref=https://www.thrillophilia.com/&amp;ap=1281&amp;be=2144&amp;fe=3560&amp;dc=2527&amp;perf=%7B%22timing%22:%7B%22of%22:1617079868806,%22n%22:0,%22u%22:2068,%22ue%22:2068,%22f%22:2,%22dn%22:2,%22dne%22:2,%22c%22:2,%22ce%22:2,%22rq%22:10,%22rp%22:2058,%22rpe%22:2149,%22dl%22:2090,%22di%22:2490,%22ds%22:2527,%22de%22:2527,%22dc%22:3558,%22l%22:3558,%22le%22:3578%7D,%22navigation%22:%7B%22ty%22:1%7D%7D&amp;fp=2337&amp;fcp=2337&amp;jsonp=NREUM.setToken"></script><script type="text/javascript" async="" src="https://script.crazyegg.com/pages/scripts/0102/8573.js?449189"></script><script src="https://js-agent.newrelic.com/nr-1208.min.js"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/ec.js"></script><script src="https://connect.facebook.net/signals/config/1080403348643757?v=2.9.33&amp;r=stable" async=""></script><script async="" src="https://connect.facebook.net/en_US/fbevents.js"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-PS75"></script><script async="">
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer', "GTM-PS75");
+  </script>
+  <!-- End Google Tag Manager -->
+
+  <!--Facebook Pixel Code-->
+  <noscript>
+    <img height="1" src="https://www.facebook.com/tr?id=1080403348643757&ev=PageView&noscript=1" style="display:none" width="1" />
+  </noscript>
+  <!--End Facebook Pixel Code-->
+
+<!-- social media meta tags  -->
+
+
+  <meta name="google-site-verification" content="pE0nrT4g4xhu9Xp3PiEMnXSXO8qbKW3RjE7zSvBNj94">
+  <meta name="sitelock-site-verification" content="9070">
+  <meta name="msvalidate.01" content="FEAE2340D4A519177A34D662B5F65BD3">
+  <meta name="alexaVerifyID" content="7ZLy3IJi8UtHZXwFTRMPXoTYneQ">
+
+<link href="/manifest.json" rel="manifest">
+
+
+
+
+
+
+    <meta charset="utf-8">
+<script>window.NREUM||(NREUM={});NREUM.info={"beacon":"bam-cell.nr-data.net","errorBeacon":"bam-cell.nr-data.net","licenseKey":"86a6d59195","applicationID":"386753053","transactionName":"cFoITEQOXlpTQxZeWF5QSVFYBVdO","queueTime":0,"applicationTime":1281,"agent":""}</script>
+<script>(window.NREUM||(NREUM={})).loader_config={licenseKey:"86a6d59195",applicationID:"386753053"};window.NREUM||(NREUM={}),__nr_require=function(e,t,n){function r(n){if(!t[n]){var i=t[n]={exports:{}};e[n][0].call(i.exports,function(t){var i=e[n][1][t];return r(i||t)},i,i.exports)}return t[n].exports}if("function"==typeof __nr_require)return __nr_require;for(var i=0;i<n.length;i++)r(n[i]);return r}({1:[function(e,t,n){function r(){}function i(e,t,n){return function(){return o(e,[u.now()].concat(c(arguments)),t?null:this,n),t?void 0:this}}var o=e("handle"),a=e(7),c=e(8),f=e("ee").get("tracer"),u=e("loader"),s=NREUM;"undefined"==typeof window.newrelic&&(newrelic=s);var d=["setPageViewName","setCustomAttribute","setErrorHandler","finished","addToTrace","inlineHit","addRelease"],p="api-",l=p+"ixn-";a(d,function(e,t){s[t]=i(p+t,!0,"api")}),s.addPageAction=i(p+"addPageAction",!0),s.setCurrentRouteName=i(p+"routeName",!0),t.exports=newrelic,s.interaction=function(){return(new r).get()};var m=r.prototype={createTracer:function(e,t){var n={},r=this,i="function"==typeof t;return o(l+"tracer",[u.now(),e,n],r),function(){if(f.emit((i?"":"no-")+"fn-start",[u.now(),r,i],n),i)try{return t.apply(this,arguments)}catch(e){throw f.emit("fn-err",[arguments,this,e],n),e}finally{f.emit("fn-end",[u.now()],n)}}}};a("actionText,setName,setAttribute,save,ignore,onEnd,getContext,end,get".split(","),function(e,t){m[t]=i(l+t)}),newrelic.noticeError=function(e,t){"string"==typeof e&&(e=new Error(e)),o("err",[e,u.now(),!1,t])}},{}],2:[function(e,t,n){function r(){return c.exists&&performance.now?Math.round(performance.now()):(o=Math.max((new Date).getTime(),o))-a}function i(){return o}var o=(new Date).getTime(),a=o,c=e(9);t.exports=r,t.exports.offset=a,t.exports.getLastTimestamp=i},{}],3:[function(e,t,n){function r(e){return!(!e||!e.protocol||"file:"===e.protocol)}t.exports=r},{}],4:[function(e,t,n){function r(e,t){var n=e.getEntries();n.forEach(function(e){"first-paint"===e.name?d("timing",["fp",Math.floor(e.startTime)]):"first-contentful-paint"===e.name&&d("timing",["fcp",Math.floor(e.startTime)])})}function i(e,t){var n=e.getEntries();n.length>0&&d("lcp",[n[n.length-1]])}function o(e){e.getEntries().forEach(function(e){e.hadRecentInput||d("cls",[e])})}function a(e){if(e instanceof m&&!g){var t=Math.round(e.timeStamp),n={type:e.type};t<=p.now()?n.fid=p.now()-t:t>p.offset&&t<=Date.now()?(t-=p.offset,n.fid=p.now()-t):t=p.now(),g=!0,d("timing",["fi",t,n])}}function c(e){d("pageHide",[p.now(),e])}if(!("init"in NREUM&&"page_view_timing"in NREUM.init&&"enabled"in NREUM.init.page_view_timing&&NREUM.init.page_view_timing.enabled===!1)){var f,u,s,d=e("handle"),p=e("loader"),l=e(6),m=NREUM.o.EV;if("PerformanceObserver"in window&&"function"==typeof window.PerformanceObserver){f=new PerformanceObserver(r);try{f.observe({entryTypes:["paint"]})}catch(v){}u=new PerformanceObserver(i);try{u.observe({entryTypes:["largest-contentful-paint"]})}catch(v){}s=new PerformanceObserver(o);try{s.observe({type:"layout-shift",buffered:!0})}catch(v){}}if("addEventListener"in document){var g=!1,w=["click","keydown","mousedown","pointerdown","touchstart"];w.forEach(function(e){document.addEventListener(e,a,!1)})}l(c)}},{}],5:[function(e,t,n){function r(e,t){if(!i)return!1;if(e!==i)return!1;if(!t)return!0;if(!o)return!1;for(var n=o.split("."),r=t.split("."),a=0;a<r.length;a++)if(r[a]!==n[a])return!1;return!0}var i=null,o=null,a=/Version\/(\S+)\s+Safari/;if(navigator.userAgent){var c=navigator.userAgent,f=c.match(a);f&&c.indexOf("Chrome")===-1&&c.indexOf("Chromium")===-1&&(i="Safari",o=f[1])}t.exports={agent:i,version:o,match:r}},{}],6:[function(e,t,n){function r(e){function t(){e(a&&document[a]?document[a]:document[i]?"hidden":"visible")}"addEventListener"in document&&o&&document.addEventListener(o,t,!1)}t.exports=r;var i,o,a;"undefined"!=typeof document.hidden?(i="hidden",o="visibilitychange",a="visibilityState"):"undefined"!=typeof document.msHidden?(i="msHidden",o="msvisibilitychange"):"undefined"!=typeof document.webkitHidden&&(i="webkitHidden",o="webkitvisibilitychange",a="webkitVisibilityState")},{}],7:[function(e,t,n){function r(e,t){var n=[],r="",o=0;for(r in e)i.call(e,r)&&(n[o]=t(r,e[r]),o+=1);return n}var i=Object.prototype.hasOwnProperty;t.exports=r},{}],8:[function(e,t,n){function r(e,t,n){t||(t=0),"undefined"==typeof n&&(n=e?e.length:0);for(var r=-1,i=n-t||0,o=Array(i<0?0:i);++r<i;)o[r]=e[t+r];return o}t.exports=r},{}],9:[function(e,t,n){t.exports={exists:"undefined"!=typeof window.performance&&window.performance.timing&&"undefined"!=typeof window.performance.timing.navigationStart}},{}],ee:[function(e,t,n){function r(){}function i(e){function t(e){return e&&e instanceof r?e:e?u(e,f,a):a()}function n(n,r,i,o,a){if(a!==!1&&(a=!0),!l.aborted||o){e&&a&&e(n,r,i);for(var c=t(i),f=v(n),u=f.length,s=0;s<u;s++)f[s].apply(c,r);var p=d[h[n]];return p&&p.push([b,n,r,c]),c}}function o(e,t){y[e]=v(e).concat(t)}function m(e,t){var n=y[e];if(n)for(var r=0;r<n.length;r++)n[r]===t&&n.splice(r,1)}function v(e){return y[e]||[]}function g(e){return p[e]=p[e]||i(n)}function w(e,t){s(e,function(e,n){t=t||"feature",h[n]=t,t in d||(d[t]=[])})}var y={},h={},b={on:o,addEventListener:o,removeEventListener:m,emit:n,get:g,listeners:v,context:t,buffer:w,abort:c,aborted:!1};return b}function o(e){return u(e,f,a)}function a(){return new r}function c(){(d.api||d.feature)&&(l.aborted=!0,d=l.backlog={})}var f="nr@context",u=e("gos"),s=e(7),d={},p={},l=t.exports=i();t.exports.getOrSetContext=o,l.backlog=d},{}],gos:[function(e,t,n){function r(e,t,n){if(i.call(e,t))return e[t];var r=n();if(Object.defineProperty&&Object.keys)try{return Object.defineProperty(e,t,{value:r,writable:!0,enumerable:!1}),r}catch(o){}return e[t]=r,r}var i=Object.prototype.hasOwnProperty;t.exports=r},{}],handle:[function(e,t,n){function r(e,t,n,r){i.buffer([e],r),i.emit(e,t,n)}var i=e("ee").get("handle");t.exports=r,r.ee=i},{}],id:[function(e,t,n){function r(e){var t=typeof e;return!e||"object"!==t&&"function"!==t?-1:e===window?0:a(e,o,function(){return i++})}var i=1,o="nr@id",a=e("gos");t.exports=r},{}],loader:[function(e,t,n){function r(){if(!E++){var e=x.info=NREUM.info,t=l.getElementsByTagName("script")[0];if(setTimeout(u.abort,3e4),!(e&&e.licenseKey&&e.applicationID&&t))return u.abort();f(h,function(t,n){e[t]||(e[t]=n)});var n=a();c("mark",["onload",n+x.offset],null,"api"),c("timing",["load",n]);var r=l.createElement("script");r.src="https://"+e.agent,t.parentNode.insertBefore(r,t)}}function i(){"complete"===l.readyState&&o()}function o(){c("mark",["domContent",a()+x.offset],null,"api")}var a=e(2),c=e("handle"),f=e(7),u=e("ee"),s=e(5),d=e(3),p=window,l=p.document,m="addEventListener",v="attachEvent",g=p.XMLHttpRequest,w=g&&g.prototype;if(d(p.location)){NREUM.o={ST:setTimeout,SI:p.setImmediate,CT:clearTimeout,XHR:g,REQ:p.Request,EV:p.Event,PR:p.Promise,MO:p.MutationObserver};var y=""+location,h={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",agent:"js-agent.newrelic.com/nr-1208.min.js"},b=g&&w&&w[m]&&!/CriOS/.test(navigator.userAgent),x=t.exports={offset:a.getLastTimestamp(),now:a,origin:y,features:{},xhrWrappable:b,userAgent:s};e(1),e(4),l[m]?(l[m]("DOMContentLoaded",o,!1),p[m]("load",r,!1)):(l[v]("onreadystatechange",i),p[v]("onload",r)),c("mark",["firstbyte",a.getLastTimestamp()],null,"api");var E=0}},{}],"wrap-function":[function(e,t,n){function r(e,t){function n(t,n,r,f,u){function nrWrapper(){var o,a,s,p;try{a=this,o=d(arguments),s="function"==typeof r?r(o,a):r||{}}catch(l){i([l,"",[o,a,f],s],e)}c(n+"start",[o,a,f],s,u);try{return p=t.apply(a,o)}catch(m){throw c(n+"err",[o,a,m],s,u),m}finally{c(n+"end",[o,a,p],s,u)}}return a(t)?t:(n||(n=""),nrWrapper[p]=t,o(t,nrWrapper,e),nrWrapper)}function r(e,t,r,i,o){r||(r="");var c,f,u,s="-"===r.charAt(0);for(u=0;u<t.length;u++)f=t[u],c=e[f],a(c)||(e[f]=n(c,s?f+r:r,i,f,o))}function c(n,r,o,a){if(!m||t){var c=m;m=!0;try{e.emit(n,r,o,t,a)}catch(f){i([f,n,r,o],e)}m=c}}return e||(e=s),n.inPlace=r,n.flag=p,n}function i(e,t){t||(t=s);try{t.emit("internal-error",e)}catch(n){}}function o(e,t,n){if(Object.defineProperty&&Object.keys)try{var r=Object.keys(e);return r.forEach(function(n){Object.defineProperty(t,n,{get:function(){return e[n]},set:function(t){return e[n]=t,t}})}),t}catch(o){i([o],n)}for(var a in e)l.call(e,a)&&(t[a]=e[a]);return t}function a(e){return!(e&&e instanceof Function&&e.apply&&!e[p])}function c(e,t){var n=t(e);return n[p]=e,o(e,n,s),n}function f(e,t,n){var r=e[t];e[t]=c(r,n)}function u(){for(var e=arguments.length,t=new Array(e),n=0;n<e;++n)t[n]=arguments[n];return t}var s=e("ee"),d=e(8),p="nr@original",l=Object.prototype.hasOwnProperty,m=!1;t.exports=r,t.exports.wrapFunction=c,t.exports.wrapInPlace=f,t.exports.argsToArray=u},{}]},{},["loader"]);</script>
+<title>Tours, Activities, Adventures &amp; Things to do | Thrillophilia</title>
+<link rel="icon" type="image/png" href="https://images.thrillophilia.com/image/upload/s--OMAkW_54--/c_fill,f_auto,fl_strip_profile,h_50,q_auto,w_50/v1/images/photos/000/112/901/original/1544797056_touch-icon-phone-152x152.png.jpg?1544797056">
+<link rel="apple-touch-icon" type="image/png" href="https://images.thrillophilia.com/image/upload/s--OMAkW_54--/c_fill,f_auto,fl_strip_profile,h_50,q_auto,w_50/v1/images/photos/000/112/901/original/1544797056_touch-icon-phone-152x152.png.jpg?1544797056">
+<link rel="apple-touch-icon" type="image/png" href="https://images.thrillophilia.com/image/upload/s--OMAkW_54--/c_fill,f_auto,fl_strip_profile,h_76,q_auto,w_76/v1/images/photos/000/112/901/original/1544797056_touch-icon-phone-152x152.png.jpg?1544797056" size="76x76">
+<link rel="apple-touch-icon" type="image/png" href="https://images.thrillophilia.com/image/upload/s--OMAkW_54--/c_fill,f_auto,fl_strip_profile,h_120,q_auto,w_120/v1/images/photos/000/112/901/original/1544797056_touch-icon-phone-152x152.png.jpg?1544797056" size="120x120">
+<link rel="apple-touch-icon" type="image/png" href="https://images.thrillophilia.com/image/upload/s--OMAkW_54--/c_fill,f_auto,fl_strip_profile,h_152,q_auto,w_152/v1/images/photos/000/112/901/original/1544797056_touch-icon-phone-152x152.png.jpg?1544797056" size="152x152">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.thrillophilia.com/">
+<meta property="og:site_name" content="https://www.thrillophilia.com/">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@Thrillophilia">
+<meta name="twitter:creator" content="@Thrillophilia">
+<meta name="language" content="english">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+<meta name="publisher" content="https://plus.google.com/b/107592018232905195780/">
+
+      <link rel="stylesheet" media="all" href="https://ui-assets-gc.thrillophilia.com/assets/home-124c906442892ce440c2c4b28a08ba36e0417d0d1a76807a6904bb49f96475a0.css" data-turbolinks-track="reload">
+
+    <script src="https://ui-assets-gc.thrillophilia.com/assets/priority-7c83289bffe3666996f826940adc784df8abf8f9c297cd472261e2a264548b94.js"></script>
+
+      <script src="https://ui-assets-gc.thrillophilia.com/assets/home-9c66a6c2a038620c6f37e102a5302ffc0c6bf7f003ac88a7c8cf6d399b6c1c44.js" defer="defer"></script>
+    
+    <link rel="preconnect" href="https://images.thrillophilia.com" crossorigin="">
+    <script src="https://kit.fontawesome.com/e1befffe2c.js" crossorigin="anonymous"></script>
---
+++ application/inc/header.php
@@ -0,0 +1,90 @@
+<header id="header" class="transparent-nav" data-scroll-white-nav="true" data-scroll-half-nav="true">
+            <div class="header-base">
+                <div class="top-panel">
+                    <div class="container">
+                        <div class="inner-wrap">
+                            <div href="#" id="header-mobile-nav-opener" class=""><span></span></div>
+                            <strong class="logo">
+                                <a href="">
+                                <img src='<?php echo base_url().'assets/images/logo.png';  ?>'>
+                                    <!-- <div class="thrillo-logo white-logo desktop-logo mobile-hidden "></div> -->
+                                    <!-- <div class="thrillo-logo colored-logo mobile-header-logo desktop-hidden"></div> -->
+                                </a>
+                            </strong>
+                            <div class="search-container">
+                                <div href="#" id="header-search-opener" class="desktop-hidden navbar-menu-toggler" data-submenu="adventureExcursionsMobileWrapper" data-menu-loaded="false" data-menu-identifier="adventure_excursions" data-version="mobile">
+                                    <span class="icon-search"></span>
+                                </div>
+                                <div class="search-slide">
+                                    <div class="inner-form-container">
+                                        <form action="/listings/search" method="get" id="global-search-form">
+                                            <fieldset>
+                                                <div class="input-wrap">
+                                                    <label for="slug-search-text" class="icon-search mobile-hidden"></label>
+                                                    <input data-destination-url="/listings/search" id="tour_destination_slug_info" name="destination_slug" type="hidden">
+                                                    <span class="twitter-typeahead" style="position: relative; display: inline-block;"><input class="string required tt-hint" size="50" type="text" readonly="" autocomplete="off" spellcheck="false" tabindex="-1" dir="ltr" style="position: absolute; top: 0px; left: 0px; border-color: transparent; box-shadow: none; opacity: 1; background: none 0% 0% / auto repeat scroll padding-box border-box rgb(255, 255, 255);"><input class="string required tt-input" id="slug-search-text" name="search" placeholder="Search Destinations, Tours, Activities" required="required" size="50" type="text" autocomplete="off" spellcheck="false" dir="auto" style="position: relative; vertical-align: top; background-color: transparent;">
+                                                        <pre aria-hidden="true" style="position: absolute; visibility: hidden; white-space: pre; font-family: Poppins, sans-serif; font-size: 9.6px; font-style: normal; font-variant: normal; font-weight: 400; word-spacing: 0px; letter-spacing: 0px; text-indent: 0px; text-rendering: auto; text-transform: none;"></pre>
+                                                        <div class="tt-menu" style="position: absolute; top: 100%; left: 0px; z-index: 100; display: none;">
+                                                            <div class="tt-dataset tt-dataset-destination_suggestions"></div>
+                                                            <div class="tt-dataset tt-dataset-sub_categories"></div>
+                                                            <div class="tt-dataset tt-dataset-tours"></div>
+                                                        </div>
+                                                    </span>
+                                                    <span class="icon-cancel-music" style="display: none" id="navSearchClear"></span>
+                                                    <button type="submit"></button>
+                                                </div>
+                                            </fieldset>
+                                        </form>
+                                        <div class="tranding-quick-list simple-carousel desktop-hidden">
+                                            <div class="slide">
+                                                <div class="tranding-quick-item header-submenu-picker navbar-menu-toggler" data-submenu="adventureExcursionsMobileWrapper" data-menu-loaded="false" data-menu-identifier="adventure_excursions" data-version="mobile">
+                                                    <div class="img-wrap">
+                                                        <img alt="" class="lazy-image" sizes="100vw" data-src="https://ui-assets-gc.thrillophilia.com/assets/ad_ex-9dde28b534e773d259619cc86ef0ceff187c7f6253ae649e4662dc5615afac96.png" data-srcset="" src="">
+                                                    </div>
+                                                    <strong class="title">Adventure Excursions</strong>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <div class="tranding-quick-item header-submenu-picker navbar-menu-toggler" data-submenu="weekendExperiencesMobileWrapper" data-menu-loaded="false" data-menu-identifier="weekend_experiences" data-version="mobile">
+                                                    <div class="img-wrap">
+                                                        <img class="lazy-image " alt="" sizes="100vw" data-src="https://ui-assets-gc.thrillophilia.com/assets/wee_ex-ac6c7ebec099b74b90b4c19115876ddf54eba26deb2b2fd9ba6c4a1e1078c4df.png" data-srcset="" src="">
+                                                    </div>
+                                                    <strong class="title">Weekend Experiences</strong>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <div class="tranding-quick-item header-submenu-picker navbar-menu-toggler" data-submenu="ToursAndActivitiesWrapper" data-menu-loaded="false" data-menu-identifier="tours_and_activities" data-version="mobile">
+                                                    <div class="img-wrap">
+                                                        <img class="lazy-image " alt="" sizes="100vw" data-src="https://ui-assets-gc.thrillophilia.com/assets/tour_act-0be79949e8c8cfce08bf65008ebb1da0380a65f433e41695459294d8919ca866.png" data-srcset="" src="">
+                                                    </div>
+                                                    <strong class="title">Tours and Activities</strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="mobile-nav-drop" class="">
+                            <ul class="mobile-tool-list">
+                                <li class="desktop-hidden"><a href="/" class="main-header-link">Home</a></li>
+                                <li class="divider desktop-hidden"></li>
+                                <li><a href="/go-to-gift-page" class="gift-link main-header-link">
+                                        
+                                        
+                                        <span class="text" style='background:#f07c7c; padding:10px;  color:white'><i class="far fa-plus-square fa-2x"></i> New Bussiness</span>
+                                    </a>
+                                </li>
+                                <li class="divider desktop-hidden"></li>
+                                <li id="header-login-wrap">
+                                    <span data-href="/consumers/sign_in" class="main-header-link trigger-login-popup onclick-link">Log In</span>
+                                </li>
+                                <li class="divider desktop-hidden"></li>
+                                <li class="desktop-hidden"><a href="https://thrillophilia.freshdesk.com/support/home" class="main-header-link">Help</a></li>
+                                <!-- <li class="desktop-hidden"><a href="#">FAQs</a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
